4d434a045f902746258d7cfce3a8b74af76481b6
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2021-01-25 17:58:40 +0000

    Creating a docker image

---
+++ go-server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Starting: Building Apps For k8s app")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Request received from %s", r.RemoteAddr)
+	fmt.Fprint(w, "Building Apps For k8s app says Hi")
+}
